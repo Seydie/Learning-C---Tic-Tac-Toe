ed0516168ea7c547bcb1df2d7a5f158b094008cf
Kayleigh van de Wiel <kelyvandewiel@hotmail.com>
2017-12-07 15:30:58 +0100

    First files

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.babout = new System.Windows.Forms.Button();
+            this.bexit = new System.Windows.Forms.Button();
+            this.bretry = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(12, 12);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(75, 75);
+            this.b1.TabIndex = 0;
+            this.b1.UseVisualStyleBackColor = true;
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(93, 12);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(75, 75);
+            this.b2.TabIndex = 1;
+            this.b2.UseVisualStyleBackColor = true;
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(174, 12);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(75, 75);
+            this.b3.TabIndex = 2;
+            this.b3.UseVisualStyleBackColor = true;
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(12, 93);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(75, 75);
+            this.b4.TabIndex = 3;
+            this.b4.UseVisualStyleBackColor = true;
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(93, 93);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(75, 75);
+            this.b5.TabIndex = 4;
+            this.b5.UseVisualStyleBackColor = true;
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(174, 93);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(75, 75);
+            this.b6.TabIndex = 5;
+            this.b6.UseVisualStyleBackColor = true;
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(12, 174);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(75, 75);
+            this.b7.TabIndex = 6;
+            this.b7.UseVisualStyleBackColor = true;
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(93, 174);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(75, 75);
+            this.b8.TabIndex = 7;
+            this.b8.UseVisualStyleBackColor = true;
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(174, 174);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(75, 75);
+            this.b9.TabIndex = 8;
+            this.b9.UseVisualStyleBackColor = true;
+            // 
+            // babout
+            // 
+            this.babout.Location = new System.Drawing.Point(12, 255);
+            this.babout.Name = "babout";
+            this.babout.Size = new System.Drawing.Size(75, 23);
+            this.babout.TabIndex = 9;
+            this.babout.Text = "About";
+            this.babout.UseVisualStyleBackColor = true;
+            this.babout.Click += new System.EventHandler(this.babout_Click);
+            // 
+            // bexit
+            // 
+            this.bexit.Location = new System.Drawing.Point(174, 255);
+            this.bexit.Name = "bexit";
+            this.bexit.Size = new System.Drawing.Size(75, 23);
+            this.bexit.TabIndex = 10;
+            this.bexit.Text = "Exit";
+            this.bexit.UseVisualStyleBackColor = true;
+            this.bexit.Click += new System.EventHandler(this.bexit_Click);
+            // 
+            // bretry
+            // 
+            this.bretry.Location = new System.Drawing.Point(93, 255);
+            this.bretry.Name = "bretry";
+            this.bretry.Size = new System.Drawing.Size(75, 23);
+            this.bretry.TabIndex = 11;
+            this.bretry.Text = "Retry";
+            this.bretry.UseVisualStyleBackColor = true;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(263, 296);
+            this.Controls.Add(this.bretry);
+            this.Controls.Add(this.bexit);
+            this.Controls.Add(this.babout);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Name = "Form";
+            this.Text = "Gekke game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button babout;
+        private System.Windows.Forms.Button bexit;
+        private System.Windows.Forms.Button bretry;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        bool Player1 = true;
+        bool Player2 = false;
+
+        public Form()
+        {
+            InitializeComponent();
+            b1.Click += ClickCheck;
+            b2.Click += ClickCheck;
+            b3.Click += ClickCheck;
+            b4.Click += ClickCheck;
+            b5.Click += ClickCheck;
+            b6.Click += ClickCheck;
+            b7.Click += ClickCheck;
+            b8.Click += ClickCheck;
+            b9.Click += ClickCheck;
+        }
+
+        private void bexit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void babout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Kayleigh van de Wiel");
+        }
+
+        private void ClickCheck(object sender, EventArgs e)
+        {
+            if (sender is Button)
+            {
+                Button b = sender as Button;
+                MessageBox.Show(b.Text);
+                b.Text = "";
+                if (1 < 0)
+                {
+                    MessageBox.Show("Gewonnen!");
+                }
+                else
+                {
+                    MessageBox.Show("Verloren!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Oops! Something went wrong!");
+                Application.Exit();
+            }
+               
+        }
+    }
+}
